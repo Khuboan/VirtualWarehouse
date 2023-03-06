730c33ee9994f1764ddcabd8c6d67d7a1608543e
1451745205 <1451745205@qq.com>
2023-03-06 21:20:00 +0800

    test

--- Assets/Scripts/mk/mktest2.cs
+++ Assets/Scripts/mk/mktest2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mktest : MonoBehaviour
+public class mktest2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
