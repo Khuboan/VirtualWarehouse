40ae731efbc3eabf80dc404d0856800be009f93f
khuboan <1779265864@qq.com>
2023-03-20 19:08:31 +0800

    小地图

--- Assets/Scripts/hbh/Scripts/minimapMgr.cs
+++ Assets/Scripts/hbh/Scripts/minimapMgr.cs
@@ -38,14 +38,17 @@ public class minimapMgr : MonoBehaviour
     public void SetPosDoor(int index)
     {
         // rootObject.warehouse[i].door[j].position[k]
-        float[] PosX, PosY;
+        float[] PosX, PosY, sizeX, sizeY;
+
         PosX = new float[doors.Length];
         PosY = new float[doors.Length];
+        sizeX = new float[doors.Length];
+        sizeY = new float[doors.Length];
         for (int i = 0; i < doors.Length; i++)
         {
             PosX[i] = (float.Parse(warehouse[index].door[i].position[1].Split(',')[0]) + float.Parse(warehouse[index].door[i].position[0].Split(',')[0])) / 2 / 10 - 100;
-            float sizeX = (float.Parse(warehouse[index].door[i].position[1].Split(',')[0]) - float.Parse(warehouse[index].door[i].position[0].Split(',')[0])) / 10;
-            doors[i].GetComponent<RectTransform>().sizeDelta = new Vector2(sizeX, 7f);
+            sizeX[i] = (float.Parse(warehouse[index].door[i].position[1].Split(',')[0]) - float.Parse(warehouse[index].door[i].position[0].Split(',')[0])) / 10;
+            doors[i].GetComponent<RectTransform>().sizeDelta = new Vector2(sizeX[i], 7f);
             //PosY[i] = (float.Parse(warehouse[0].door[i].position[1].Split(',')[1]) + float.Parse(warehouse[0].door[i].position[2].Split(',')[1])) / 2 / 10 - 50;
             doors[i].GetComponent<RectTransform>().localPosition = new Vector3(PosX[i], -rectHeight / 2,0);
         }
@@ -57,41 +60,54 @@ public class minimapMgr : MonoBehaviour
 
     public void SetPosBarrier(int index)
     {
-        float[] PosX,PosY;
+        float[] PosX,PosY, sizeX, sizeY;
         PosX = new float[barrier.Length];
         PosY = new float[barrier.Length];
+        sizeX = new float[barrier.Length];
+        sizeY = new float[barrier.Length];
         for (int i = 0; i < barrier.Length; i++)
         {
             PosX[i] = (float.Parse(warehouse[index].barrier[i].position[1].Split(',')[0]) + float.Parse(warehouse[index].barrier[i].position[0].Split(',')[0])) / 2 / 10 - 100;
             PosY[i] = (float.Parse(warehouse[index].barrier[i].position[1].Split(',')[1]) + float.Parse(warehouse[index].barrier[i].position[2].Split(',')[1])) / 2 / 10 - 50;
+            sizeX[i] = (float.Parse(warehouse[index].barrier[i].position[1].Split(',')[0]) - float.Parse(warehouse[index].barrier[i].position[0].Split(',')[0])) / 10;
+            sizeY[i] = (float.Parse(warehouse[index].barrier[i].position[2].Split(',')[1]) - float.Parse(warehouse[index].barrier[i].position[1].Split(',')[1])) / 10;
+            barrier[i].GetComponent<RectTransform>().sizeDelta = new Vector2(sizeX[i],sizeY[i]);
             barrier[i].GetComponent<RectTransform>().localPosition = new Vector3(PosX[i], PosY[i]);
         }
     }
 
     public void SetPosBin(int index)
     {
-        float[] PosX, PosY;
+        float[] PosX, PosY, sizeX, sizeY;
         PosX = new float[bin.Length];
         PosY = new float[bin.Length];
+        sizeX = new float[bin.Length];
+        sizeY = new float[bin.Length];
         for (int i = 0; i < bin.Length; i++)
         {
             PosX[i] = (float.Parse(warehouse[index].bin[i].position[1].Split(',')[0]) + float.Parse(warehouse[index].bin[i].position[0].Split(',')[0])) / 2 / 10 - 100;
             PosY[i] = (float.Parse(warehouse[index].bin[i].position[1].Split(',')[1]) + float.Parse(warehouse[index].bin[i].position[2].Split(',')[1])) / 2 / 10 - 50;
-            //Debug.Log(PosX[i]);
-            //Debug.Log(PosY[i]);
+            sizeX[i] = (float.Parse(warehouse[index].bin[i].position[1].Split(',')[0]) - float.Parse(warehouse[index].bin[i].position[0].Split(',')[0])) / 10;
+            sizeY[i] = (float.Parse(warehouse[index].bin[i].position[2].Split(',')[1]) - float.Parse(warehouse[index].bin[i].position[1].Split(',')[1])) / 10;
+            bin[i].GetComponent<RectTransform>().sizeDelta = new Vector2(sizeX[i], sizeY[i]);
             bin[i].GetComponent<RectTransform>().localPosition = new Vector3(PosX[i], PosY[i]);
         }
     }
 
     public void SetPosShelfs(int index)
     {
-        float[] PosX, PosY;
+        float[] PosX, PosY, sizeX, sizeY;
         PosX = new float[shelfs.Length];
         PosY = new float[shelfs.Length];
+        sizeX = new float[shelfs.Length];
+        sizeY = new float[shelfs.Length];
         for (int i = 0; i < shelfs.Length; i++)
         {
             PosX[i] = (float.Parse(warehouse[index].shelf[i].position[1].Split(',')[0]) + float.Parse(warehouse[index].shelf[i].position[0].Split(',')[0])) / 2 / 10 - 100;
             PosY[i] = (float.Parse(warehouse[index].shelf[i].position[1].Split(',')[1]) + float.Parse(warehouse[index].shelf[i].position[2].Split(',')[1])) / 2 / 10 - 50;
+            sizeX[i] = (float.Parse(warehouse[index].shelf[i].position[1].Split(',')[0]) - float.Parse(warehouse[index].shelf[i].position[0].Split(',')[0])) / 10;
+            sizeY[i] = (float.Parse(warehouse[index].shelf[i].position[2].Split(',')[1]) - float.Parse(warehouse[index].shelf[i].position[1].Split(',')[1])) / 10;
+            shelfs[i].GetComponent<RectTransform>().sizeDelta = new Vector2(sizeX[i], sizeY[i]);
             shelfs[i].GetComponent<RectTransform>().localPosition = new Vector3(PosX[i], PosY[i]);
         }
     }
