b2c4ddfd6a860c222487e40262901a26e111cc4f
khuboan <1779265864@qq.com>
2023-03-20 11:40:52 +0800

    小地图

--- Assets/Scripts/hbh/Scripts/minimapMgr.cs
+++ Assets/Scripts/hbh/Scripts/minimapMgr.cs
@@ -7,6 +7,8 @@ public class minimapMgr : MonoBehaviour
 {
     [HideInInspector]
     public Image rectLine;
+    [HideInInspector]
+    public Image fDoor, bDoor, lDoor, rDoor;
 
     public float rectHeight;
     public float rectWidth;
@@ -15,5 +17,14 @@ public class minimapMgr : MonoBehaviour
     public void SetRectSize()
     {
         rectLine.GetComponent<RectTransform>().sizeDelta = new Vector2(rectWidth, rectHeight);
+        SetPosDoor();
+    }
+
+    public void SetPosDoor()
+    {
+        bDoor.GetComponent<RectTransform>().localPosition = new Vector3(0, -rectHeight / 2, 0);
+        fDoor.GetComponent<RectTransform>().localPosition = new Vector3(0, rectHeight / 2, 0);
+        lDoor.GetComponent<RectTransform>().localPosition = new Vector3(-rectWidth / 2, 0, 0);
+        rDoor.GetComponent<RectTransform>().localPosition = new Vector3(rectWidth / 2, 0, 0);
     }
 }
