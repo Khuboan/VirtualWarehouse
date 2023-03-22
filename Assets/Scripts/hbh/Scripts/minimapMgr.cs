4d109495733cdc2d7b95c4fb1d8491ba60bfbc2f
Byou1234 <byou2021@163.com>
2023-03-22 23:25:49 +0800

    脚本bug

--- Assets/Scripts/hbh/Scripts/minimapMgr.cs
+++ Assets/Scripts/hbh/Scripts/minimapMgr.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MinimapMgr : MonoBehaviour
+public class minimapMgr : MonoBehaviour
 {
     private Transform transWarehouses;
     Warehouse[] warehouse;
