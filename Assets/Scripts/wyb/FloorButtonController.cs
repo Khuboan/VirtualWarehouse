90771ab400e39653ac7757624676b62060db8e5e
1451745205 <1451745205@qq.com>
2023-03-23 12:27:21 +0800

    货架信息窗口完毕

--- Assets/Scripts/wyb/FloorButtonController.cs
+++ Assets/Scripts/wyb/FloorButtonController.cs
@@ -4,36 +4,42 @@ using UnityEngine.UI;
 public class FloorButtonController : MonoBehaviour
 {
     private GameObject targetObject;   // 需要控制的GameObject
+    private GameObject table;
     public static string buttonText;
 
     private void Start()
     {
-         targetObject = GameObject.Find("UI/ShelfInfoWindow/Shelf_Goods/FloorInfo");
-         
+        targetObject = GameObject.Find("UI/ShelfInfoWindow/Shelf_Goods/FloorInfo");
+        table = GameObject.Find("UI/ShelfInfoWindow/Shelf_Goods/FloorInfo/Panel/Table");
     }
-    private void Update()
-    {
-
-    }
-
     /// <summary>
     /// 按钮点击事件
     /// </summary>
     public void ClickButton()
     {
-        //判断列表状态
-        if(targetObject.activeSelf == false)
-        {
-            targetObject.SetActive(true);
-        }
-        else
-        {
-            targetObject.SetActive(false);
-        }
-
+        //层数按钮原名字
         buttonText = gameObject.transform.GetChild(0).GetComponent<Text>().text;
 
         //将列表显示在所点击的按钮下方
         targetObject.transform.position = gameObject.transform.position - new Vector3(0, 20, 0);
+
+        //判断列表状态
+        if (targetObject.activeSelf == false)
+        {
+            targetObject.SetActive(true);
+
+            //创建列表信息
+            SelfInfoGet.selfInfoGet.CreateTable(SelfInfoGet.shelfHub);
+        }
+        else
+        {
+            targetObject.SetActive(false);
+            //销毁原来的列表
+            //如果层数按钮名字发生变化
+            for (int i = 1; i < table.transform.childCount; i++)
+            {
+                Destroy(table.transform.GetChild(i).gameObject);
+            }
+        }
     }
 }
--- Assets/Scripts/wyb/SelfInfoGet.cs
+++ Assets/Scripts/wyb/SelfInfoGet.cs
@@ -5,20 +5,27 @@ using UnityEngine.UI;
 
 public class SelfInfoGet : MonoBehaviour
 {
+    public static SelfInfoGet selfInfoGet;
+
     public GameObject Row_Prefab;   //表头预设
     public GameObject FloorButtonPre;   //按钮预设
     public GameObject ButtonGroup;    //按钮父物体
     public GameObject Panel;   //每层货物详细列表
     public Text ShelfName, ShelfIndex, ShelfFloor;
+    public static ShelfHub shelfHub;
 
     void Start()
     {
         
     }
+    private void Awake()
+    {
+        selfInfoGet = this;
+    }
 
     void Update()
     {
-        ShelfHub shelfHub = CameraController.instance.targetPoint[CameraController.instance.camposIndex].parent.parent.GetComponent<ShelfHub>();
+        shelfHub = CameraController.instance.targetPoint[CameraController.instance.camposIndex].parent.parent.GetComponent<ShelfHub>();
         //货架原名字
         string shelfName = ShelfName.text;
         //显示货架名字，编号，层数
@@ -35,13 +42,6 @@ public class SelfInfoGet : MonoBehaviour
                 Destroy(ButtonGroup.transform.GetChild(i).gameObject);
             }
             CreateButton(shelfHub);
-        }
-
-        //生成货物信息的列表
-        GameObject floorInfo = GameObject.Find("UI/ShelfInfoWindow/Shelf_Goods/FloorInfo");
-        if (floorInfo.activeSelf == true)
-        {
-            CreateTable(shelfHub);
         }
     }
 
@@ -71,10 +71,15 @@ public class SelfInfoGet : MonoBehaviour
     /// <param name="shelfHub"></param>
     public void CreateTable(ShelfHub shelfHub)
     {
-        //获取货架层数
-        int shelfFloor = shelfHub.shelf.floor.Count;
+        //获取到当前鼠标点击按钮，即要查看的层数
+        string[] arry = FloorButtonController.buttonText.Trim().Split('层');
+        int nowFloor = int.Parse(arry[0]);
+        //Debug.LogError("内容 = " + FloorButtonController.buttonText.Trim() + "  长度= " + arry.Length + "层数 = " + shelfHub.shelf.floor.Count  + "当前层级 = " + nowFloor);
+        //Debug.LogError("货物数量 = " + shelfHub.shelf.floor[nowFloor].material.Count);
+
         //生成列表数据
-        for (int i = 0; i < shelfHub.shelf.floor[shelfFloor].material.Count; i++)
+
+        for (int i = 0; i < shelfHub.shelf.floor[nowFloor-1].material.Count; i++)
         {
             GameObject table = GameObject.Find("UI/ShelfInfoWindow/Shelf_Goods/FloorInfo/Panel/Table");
             GameObject row = GameObject.Instantiate(Row_Prefab, table.transform.position, table.transform.rotation) as GameObject;
@@ -82,18 +87,12 @@ public class SelfInfoGet : MonoBehaviour
             row.transform.SetParent(table.transform);
             row.transform.localScale = Vector3.one;//设置缩放比例1,1,1，不然默认的比例非常大
 
-            //获取到当前鼠标点击按钮，即要查看的层数
-            string[] arry = FloorButtonController.buttonText.Trim().Split('层');
-            int nowFloor = int.Parse(arry[0]);
-
             //设置预设实例中的各个子物体的文本内容
-            material material = shelfHub.shelf.floor[nowFloor].material[i];
+            material material = shelfHub.shelf.floor[nowFloor-1].material[i];
             FindChildGameObject(row.gameObject, "Cell").GetComponent<Text>().text = material.name;
             FindChildGameObject(row.gameObject, "Cell1").GetComponent<Text>().text = material.code;
-            FindChildGameObject(row.gameObject, "Cell2").GetComponent<Text>().text = material.count.ToString()+ material.unit;
+            FindChildGameObject(row.gameObject, "Cell2").GetComponent<Text>().text = material.count.ToString() + material.unit;
         }
-        
-
     }
 
     /// <summary>
