55fd3d8f79deb537877b4cb535ad2b8b1825f744
khuboan <1779265864@qq.com>
2023-03-24 17:03:02 +0800

    箱子视角

--- Assets/Scripts/hbh/Scripts/BtnShelf.cs
+++ Assets/Scripts/hbh/Scripts/BtnShelf.cs
@@ -31,6 +31,27 @@ public class BtnShelf : MonoBehaviour
         }
     }
 
+    public void BinPosCam()
+    {
+        int index = ToolExtractData(this.name);
+
+        int houseIndex = ToolExtractData(this.transform.parent.parent.name);
+
+        GameObject HouseHub = GameObject.Find("HouseHub");
+        Transform shelfPosCam = HouseHub.transform.Find("House" + houseIndex).Find("Bin" + index).GetComponent<BinHub>().CenterPos;
+        Debug.Log(shelfPosCam.position);
+        for (int i = 0; i < CameraController.instance.targetPoint.Count; i++)
+        {
+            if (shelfPosCam == CameraController.instance.targetPoint[i])
+            {
+                CameraController.instance.camposIndex = i;
+                CameraController.instance.isMoveDone = false;
+                CameraController.instance.CameraMoveTime = 0;
+                CameraController.instance.campos = CameraController.instance.transform.position;
+            }
+        }
+    }    
+
 
     public static int ToolExtractData(string str)
     {
