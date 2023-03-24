de059d28645541b9a7259b987a378420a011bfc2
khuboan <1779265864@qq.com>
2023-03-24 16:49:19 +0800

    小地图动态扩容

--- Assets/Scripts/hbh/Scripts/minimapMgr.cs
+++ Assets/Scripts/hbh/Scripts/minimapMgr.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,6 +16,9 @@ public class minimapMgr : MonoBehaviour
 
     public Button btnReset;
     public Button btnMiniMap;
+
+    private float mapWidth;
+    private float mapHeight;
     void Start()
     {
         transWarehouses = transform.Find("Warehouses");
@@ -45,12 +49,27 @@ public class minimapMgr : MonoBehaviour
         wareCount = warehouse.Length;
         GameObject[] gos = new GameObject[wareCount];
 
+        List<float> mapX = new List<float>();//存储所有仓库位置X
+        List<float> mapY = new List<float>();//存储所有仓库位置Y
         for (int i = 0; i < wareCount; i++)
         {
+            for (int j = 0; j <4; j++)
+            {
+                mapX.Add(int.Parse(warehouse[i].position[j].Split(',')[0]));
+                mapY.Add(int.Parse(warehouse[i].position[j].Split(',')[1]));
+            }
+
             gos[i] = GameObject.Instantiate(Resources.Load<GameObject>("Warehouses/house"), transWarehouses);
             gos[i].name = "house" + i;
             gos[i].transform.Find("txtHouse").GetComponent<Text>().text = warehouse[i].name;
         }
+
+        mapWidth = mapX.Max() - mapX.Min();
+        mapHeight = mapY.Max() - mapY.Min();
+
+        //小地图大小
+        this.transform.parent.GetComponent<RectTransform>().sizeDelta = new Vector2(mapWidth / 10 + 200, mapHeight / 10 + 200);
+        this.transform.parent.GetComponent<RectTransform>().localPosition = new Vector3(-(mapWidth / 10 + 200) / 2, (mapHeight / 10 + 200) / 2);
 
         warehousesMgrs = new WarehousesMgr[wareCount];
         housePosX = new float[wareCount];
@@ -62,8 +81,12 @@ public class minimapMgr : MonoBehaviour
             housePosY[i] = (float.Parse(warehouse[i].position[2].Split(',')[1]) + float.Parse(warehouse[i].position[1].Split(',')[1])) /2;
 
             warehousesMgrs[i] = gos[i].GetComponent<WarehousesMgr>();
-            warehousesMgrs[i].GetComponent<RectTransform>().localPosition = new Vector3(housePosX[i]/10-100, housePosY[i] / 10 - 100, 0);
+            warehousesMgrs[i].GetComponent<RectTransform>().localPosition =
+                new Vector3((housePosX[i] / 10 - 100) - ((mapWidth / 10 / 200) - 1) * 100,
+                (housePosY[i] / 10 - 100) - ((mapHeight / 10 / 200) - 1) * 100, 0);
         }
+
+      
     }
 
     public void WarehousesCreat()
